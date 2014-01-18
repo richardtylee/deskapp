d6938930bd61fba77144d6ac4557f6e58b6e86bc
richardtylee <richardtylee@gmail.com>
2014-01-18 02:37:34 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deskapp::Application.config.secret_key_base = 'e3f0cbe82f9828948bcc53bf3b107ef0141ed42b70de86fc6f1d483927169f159080411241162188bf91430dc8ab608d3aa81b86a1f4d8007962834cb9aed91b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Deskapp::Application.config.session_store :cookie_store, key: '_deskapp_session'
